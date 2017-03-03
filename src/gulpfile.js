e7dccf4cc38f04beb78d37f348af18fa2360a7f3
charly-poket <alu0100844665@ull.edu.es>
2017-03-03 20:12:29 +0000

    Añadiendo basic_routing al gulp

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -2,9 +2,13 @@ var gulp = require('gulp');
 var shell = require('gulp-shell');
 var ghPages = require('gulp-gh-pages');
 
-gulp.task('server_routing', function(){
-  return gulp.src('./src').pipe(shell(['node /routing_guide/server_routing.js']))
+gulp.task('basic_routing', function(){
+  return gulp.src('./src').pipe(shell(['node /basic_routing/basic_routing.js']))
   });
+
+gulp.task('server_routing', function(){
+    return gulp.src('./src').pipe(shell(['node /routing_guide/server_routing.js']))
+    });
 
 gulp.task('server_middleware_basico', function(){
   return gulp.src('./src').pipe(shell(['node /middleware/server_middleware_basico.js']))
